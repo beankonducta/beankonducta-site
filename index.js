3149cf57d5ce6186c0e3799f84647d7d5ee74c19
patrick andrews <patrickxandrews@gmail.com>
2023-07-06 09:02:48 -0600

    make much more mobile friendly

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import b from './assets/b.glb';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -19,7 +18,7 @@ const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 directionalLight.position.set( 0, 100, 0 );
 scene.add( directionalLight );
 
-loader.load(b, function (gltf) {
+loader.load('https://firebasestorage.googleapis.com/v0/b/beankonducta-site.appspot.com/o/b.glb?alt=media&token=8ab0461a-a5c2-48b3-b976-9db6dd6e3b25', function (gltf) {
     b = gltf.scene;
     b.scale.set(50, 50, 50);
     scene.add(b);
@@ -29,6 +28,17 @@ loader.load(b, function (gltf) {
     console.error(error);
 
 });
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
 
 function animate() {
     requestAnimationFrame(animate);
